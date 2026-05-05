d916678b97b2c358d3989be8755ffee3a10b1ab2
brew_howzit <keegzbrady08@gmail.com>
2026-05-05 11:45:33 +0700

    Work sync: Draw player ship

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame, constants
+from logger import log_state
+from player import Player
+
+
+
+def main():
+    print(f"Starting Asteroids with pygame version {pygame.version.ver}")
+
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(f"{dt}") 
+        
+
+if __name__ == "__main__":
+    main()
